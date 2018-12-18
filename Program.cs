43332fcbaa6b1cfc7e31bc06d0f5545f9bc5cd8d
Eric Rozell <erozell@outlook.com>
2018-12-18 16:10:32 -0500

    Fix main entry point (req'd for netcoreapp2.1)

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.Demo
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
