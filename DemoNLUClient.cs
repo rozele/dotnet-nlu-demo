f30a68598a4756ec8a7cf49b5b37772660001214
Eric Rozell <erozell@outlook.com>
2019-08-06 22:23:22 -0400

    Updates to NLU.DevOps v0.3

--- DemoNLUClient.cs
+++ DemoNLUClient.cs
@@ -8,12 +8,13 @@ namespace NLU.DevOps.Demo
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using Core;
     using Models;
     using Newtonsoft.Json;
 
-    internal sealed class DemoNLUService : INLUService
+    internal class DemoNLUClient : DefaultNLUTestClient, INLUTrainClient
     {
-        public DemoNLUService(string trainedUtterances)
+        public DemoNLUClient(string trainedUtterances)
         {
             this.Utterances = new List<LabeledUtterance>();
             if (trainedUtterances != null)
@@ -29,19 +30,8 @@ namespace NLU.DevOps.Demo
 
         public Task TrainAsync(IEnumerable<LabeledUtterance> utterances, CancellationToken cancellationToken)
         {
-            this.Utterances.AddRange(utterances);
+            Utterances.AddRange(utterances);
             return Task.CompletedTask;
-        }
-
-        public Task<LabeledUtterance> TestAsync(string utterance, CancellationToken cancellationToken)
-        {
-            var matchedUtterance = this.Utterances.FirstOrDefault(u => u.Text == utterance);
-            return Task.FromResult(matchedUtterance ?? new LabeledUtterance(null, null, null));
-        }
-
-        public Task<LabeledUtterance> TestSpeechAsync(string speechFile, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
         }
 
         public Task CleanupAsync(CancellationToken cancellationToken)
@@ -49,7 +39,18 @@ namespace NLU.DevOps.Demo
             return Task.CompletedTask;
         }
 
-        public void Dispose()
+        protected override Task<LabeledUtterance> TestAsync(string utterance, CancellationToken cancellationToken)
+        {
+            var matchedUtterance = Utterances.FirstOrDefault(u => u.Text == utterance);
+            return Task.FromResult(matchedUtterance ?? new LabeledUtterance(null, null, null));
+        }
+
+        protected override Task<LabeledUtterance> TestSpeechAsync(string speechFile, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
         {
         }
     }
---
+++ DemoNLUClientFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.Demo
+{
+    using System.Composition;
+    using Microsoft.Extensions.Configuration;
+    using Models;
+
+    [Export("demo", typeof(INLUClientFactory))]
+    public class DemoNLUClientFactory : INLUClientFactory
+    {
+        public INLUTrainClient CreateTrainInstance(IConfiguration configuration, string settingsPath)
+        {
+            return new DemoNLUClient(configuration["trainedUtterances"]);
+        }
+
+        public INLUTestClient CreateTestInstance(IConfiguration configuration, string settingsPath)
+        {
+            return new DemoNLUClient(configuration["trainedUtterances"]);
+        }
+    }
+}
